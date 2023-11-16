698b23a48fbe188e4830f9da8ca0af1e3e41de87
Robertjo97 <robertjo97@gmail.com>
2023-11-16 00:14:23 -0800

    flipped match history

--- leaderboard.js
+++ leaderboard.js
@@ -73,10 +73,10 @@ function buildMatchHistory(playerData) {
     let table = document.createElement('table');
     matchHistory.appendChild(table);
 
-    for (let i = -1; i < playerData[0].matchHistory.length; i++) { //playerData should actually be signed in user
+    for (let i = playerData[0].matchHistory.length; i >= 0; i--) { //playerData should actually be signed in user
         let tr = document.createElement('tr');
         table.appendChild(tr);
-        if (i === -1) {
+        if (i === playerData[0].matchHistory.length) {
             tr.innerHTML = '<th>Match</th><th>Winner</th><th>Loser</th><th>Score</th><th>Time</th>';
         }
         else {
@@ -123,6 +123,57 @@ function buildMatchHistory(playerData) {
             }
         }
     }
+
+    // for (let i = -1; i < playerData[0].matchHistory.length; i++) { //playerData should actually be signed in user
+    //     let tr = document.createElement('tr');
+    //     table.appendChild(tr);
+    //     if (i === -1) {
+    //         tr.innerHTML = '<th>Match</th><th>Winner</th><th>Loser</th><th>Score</th><th>Time</th>';
+    //     }
+    //     else {
+    //         for (let j = 0; j < 5; j++) {
+    //             let td = document.createElement('td');
+    //             switch (j) {
+    //                 case 0:
+    //                     td.innerHTML = 'Match ' + playerData[0].matchHistory[i].match;
+    //                     break;
+
+    //                 case 1:
+    //                     if (playerData[0].matchHistory[i].winner == true) {
+    //                         td.innerHTML = 'Rob'; //change to signed in player 
+    //                     }
+    //                     else {
+    //                         td.innerHTML = 'Player 2';
+    //                     }
+    //                     break;
+
+    //                 case 2:
+    //                     if (playerData[0].matchHistory[i].winner == false) {
+    //                         td.innerHTML = 'Rob'; //change
+    //                     }
+    //                     else {
+    //                         td.innerHTML = 'Player 2';
+    //                     }
+    //                     break;
+    //                 case 3:
+    //                     if (playerData[0].matchHistory[i].winner == true) {
+    //                         td.innerHTML = playerData[0].matchHistory[i].totalScore + ' : ' + playerData[0].matchHistory[i].opScore;
+    //                     }
+    //                     else {
+    //                         td.innerHTML = playerData[0].matchHistory[i].opScore + ' : ' + playerData[0].matchHistory[i].totalScore;
+    //                     }
+    //                     break;
+    //                 case 4:
+    //                     let rawSeconds = playerData[0].matchHistory[i].timePlayed;
+    //                     let minutes = Math.floor(rawSeconds / 60);
+    //                     let seconds = rawSeconds - minutes * 60;
+    //                     td.innerHTML = minutes + ' minutes ' + seconds + ' seconds';
+    //                     break;
+    //             }
+    //             tr.appendChild(td);
+    //         }
+    //     }
+    // }
 
 }
 
