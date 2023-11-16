665c4053915484dbf9fffdc0cf379a29160aa583
Robertjo97 <robertjo97@gmail.com>
2023-11-15 23:16:16 -0800

    Modify leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -1,5 +1,5 @@
 let leaderboard = document.getElementById('leaderboard');
-let matchHistory = document.getElementById('mathHistory');
+let matchHistory = document.getElementById('matchHistory');
 
 function getPlayerData(func) {
     let request = new XMLHttpRequest();
@@ -14,8 +14,13 @@ function getPlayerData(func) {
 }
 
 function buildTable(playerData) {
+    let title = document.createElement('h3');
+    title.innerHTML= 'Leaderboard';
+    leaderboard.appendChild(title);
+
     let table = document.createElement('table');
     leaderboard.appendChild(table);
+    
     for (let i = -1; i < playerData.length; i++) {
         let tr = document.createElement('tr');
         table.appendChild(tr);
@@ -23,7 +28,7 @@ function buildTable(playerData) {
             tr.innerHTML = '<th>Username</th><th>Total Score</th><th>Games Won</th><th>Time Played</th><th>Games Played</th>';
         }
         else {
-            for (let j = 0; j < 6; j++) {
+            for (let j = 0; j < 5; j++) {
                 let td = document.createElement('td');
                 switch (j) {
                     case 0:
@@ -42,16 +47,16 @@ function buildTable(playerData) {
                         break;
 
                     case 3:
+                        td.innerHTML = playerData[i].gamesPlayed;
+                        tr.appendChild(td);
+                        break;
+
+                    case 4:
                         let rawSeconds = playerData[i].timePlayed;
                         let minutes = Math.floor(rawSeconds / 60);
                         let seconds = rawSeconds - minutes * 60;
 
                         td.innerHTML = minutes + ' minutes ' + seconds + ' seconds';
-                        tr.appendChild(td);
-                        break;
-
-                    case 4:
-                        td.innerHTML = playerData[i].gamesPlayed;
                         tr.appendChild(td);
                         break;
                 }
@@ -61,9 +66,65 @@ function buildTable(playerData) {
 }
 
 function buildMatchHistory(playerData) {
+    let title = document.createElement('h3');
+    title.innerHTML = 'Match History';
+    matchHistory.appendChild(title);
+
     let table = document.createElement('table');
     matchHistory.appendChild(table);
-    
+
+    for (let i = -1; i < playerData[0].matchHistory.length; i++) { //playerData should actually be signed in user
+        let tr = document.createElement('tr');
+        table.appendChild(tr);
+        if (i === -1) {
+            tr.innerHTML = '<th>Match</th><th>Winner</th><th>Loser</th><th>Score</th><th>Time</th>';
+        }
+        else {
+            for (let j = 0; j < 5; j++) {
+                let td = document.createElement('td');
+                switch (j) {
+                    case 0:
+                        td.innerHTML = 'Match ' + playerData[0].matchHistory[i].match;
+                        break;
+
+                    case 1:
+                        if (playerData[0].matchHistory[i].winner == true) {
+                            td.innerHTML = 'Rob'; //change to signed in player 
+                        }
+                        else {
+                            td.innerHTML = 'Player 2';
+                        }
+                        break;
+
+                    case 2:
+                        if (playerData[0].matchHistory[i].winner == false) {
+                            td.innerHTML = 'Rob'; //change
+                        }
+                        else {
+                            td.innerHTML = 'Player 2';
+                        }
+                        break;
+                    case 3:
+                        if (playerData[0].matchHistory[i].winner == true) {
+                            td.innerHTML = playerData[0].matchHistory[i].totalScore + ' : ' + playerData[0].matchHistory[i].opScore;
+                        }
+                        else {
+                            td.innerHTML = playerData[0].matchHistory[i].opScore + ' : ' + playerData[0].matchHistory[i].totalScore;
+                        }
+                        break;
+                    case 4:
+                        let rawSeconds = playerData[0].matchHistory[i].timePlayed;
+                        let minutes = Math.floor(rawSeconds / 60);
+                        let seconds = rawSeconds - minutes * 60;
+                        td.innerHTML = minutes + ' minutes ' + seconds + ' seconds';
+                        break;
+                }
+                tr.appendChild(td);
+            }
+        }
+    }
+
 }
 
 getPlayerData(buildTable);
+getPlayerData(buildMatchHistory);
