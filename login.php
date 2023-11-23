a1b78c61d67f06716d8ffc20aa94074345438836
Robertjo97 <robertjo97@gmail.com>
2023-11-23 14:32:16 -0800

    Convert login to SQL

--- login.php
+++ login.php
@@ -1,20 +1,50 @@
 <?php
-$rawData = file_get_contents('./users.json');
-$data = json_decode($rawData);
-$size = count($data);
-$username = null;
-$password = null;
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        if(isset($_POST['username']) && isset($_POST['password'])){
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            for($i = 0; $i < $size; $i++){
-                if($data[$i]->username == $username && $data[$i]->password == $password){
-                    header('Location: ./game1.html');
-                    exit;
-                }
+session_start();
+$_SESSION['username'] = null;
+
+$servername = 'localhost';
+$username = 'Checkers';
+$password = 'CSCI130Checkers_';
+$dbName = 'gameData';
+
+$user = null;
+$pass = null;
+
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+if($conn->connect_error){
+    die("Error: " . $conn->error);
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST["username"]) && isset($_POST["password"])){
+        $user = $_POST["username"];
+        $pass = $_POST["password"];
+
+        $sql = "SELECT user_id, username, password FROM users WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $user);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        
+        if($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+            if($pass == $row["password"]){
+                $_SESSION["username"] = $row["username"];
+                $_SESSION["user_id"] = $row["user_id"];
+                header("Location: ./menu.php");
+                exit;
             }
-            echo 'fail';
+            else {
+                echo "Invalid credentials";
+            }
         }
+        else {
+            echo "Invalid credentials";
+        }
+        $stmt->close();
     }
+}
+$conn->close();
 ?>
--- register.php
+++ register.php
@@ -10,44 +10,46 @@ $user = null;
 $pass = null;
 
 $conn = new mysqli($servername, $username, $password, $dbName);
-if($conn->connect_error){
+if ($conn->connect_error) {
     die("Error: " . $conn->connect_error);
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST["username"]) && isset($_POST["password"])) {
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
 
-    if($user == ' ' || $pass == ' '){
-        echo "Error: invalid username or password";
-    }
-    else {
-        $sql = "SELECT * FROM users WHERE username = ?";
+        if ($user == ' ' || $pass == ' ') {
+            echo "Error: invalid username or password";
+        } else {
+            $sql = "SELECT * FROM users WHERE username = ?";
 
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("s", $user);
-        $stmt->execute();
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $user);
+            $stmt->execute();
 
-        $result = $stmt->get_result();
+            $result = $stmt->get_result();
 
-        $stmt->close();
+            $stmt->close();
 
-        if($result->num_rows > 0) {
-            echo "Error: User already exists";
-        }
-        else if ($result->num_rows == 0){
-            $insert = "INSERT INTO users (username, password, totalScore, gamesWon, timePlayed, gamesPlayed) VALUES (?, ?, 0, 0, 0, 0 )";
+            if ($result->num_rows > 0) {
+                echo "Error: User already exists";
+            } else if ($result->num_rows == 0) {
+                $insert = "INSERT INTO users (username, password, totalScore, gamesWon, timePlayed, gamesPlayed) VALUES (?, ?, 0, 0, 0, 0 )";
 
-            $insertstmt = $conn->prepare($insert);
-            $insertstmt->bind_param("ss", $user, $pass);
-            if($insertstmt->execute()){
-                $_SESSION["username"] = $user;
-                header("Location: ./game1.html");
+                $insertstmt = $conn->prepare($insert);
+                $insertstmt->bind_param("ss", $user, $pass);
+                if ($insertstmt->execute()) {
+                    $id = $conn->insert_id;
+                    $_SESSION["username"] = $user;
+                    $_SESSION["user_id"] = $id;
+                    header("Location: ./game1.php");
+                    exit;
+                } else {
+                    echo "Error: " . $insertstmt->error;
+                }
+                $insertstmt->close();
             }
-            else{
-                echo "Error: " . $insertstmt->error;
-            }
-            $insertstmt->close();
         }
     }
 }
