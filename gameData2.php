856473496194a9ed9a05540abed508d3665f70ad
Robertjo97 <robertjo97@gmail.com>
2023-11-23 18:32:17 -0800

    Add numMatches

--- gameData2.php
+++ gameData2.php
@@ -19,12 +19,24 @@ if ($conn->connect_error) {
     die('Error: ' . $conn->connect_error);
 }
 
+$sql = "SELECT * FROM users WHERE username = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $user);
+$stmt->execute();
+
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $numMatches = $row['COUNT(*)'];
+} else {
+    $numMatches = 0;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     if (isset($_POST["totalScore"]) && isset($_POST["timePlayed"]) && isset($_POST["opScore"]) && isset(["$win"])) {
         $totalScore = $_POST['totalScore'];
         $timePlayed = $_POST['timePlayed'];
         $opScore = $_POST['opScore'];
+        echo 'Request Received. Currently signed in user: ' . $user . ' ' . $totalScore . " " . $timePlayed . " " . $opScore . " " . '$numMatches goes here' . " " . $win;
     }
-
-    echo 'Request Received. Currently signed in user: ' . $user . ' ' . $totalScore . " " . $timePlayed . " " . $opScore . " " . '$numMatches goes here' . " " . $win;
 }
