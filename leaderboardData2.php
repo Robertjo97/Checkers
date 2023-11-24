9c4bdd6e58b1730988535765faede50d07578bd2
Robertjo97 <robertjo97@gmail.com>
2023-11-24 12:21:15 -0800

    Add matchHistoryData.php

--- leaderboardData2.php
+++ leaderboardData2.php
@@ -6,18 +6,14 @@ $username = 'Checkers';
 $password = 'CSCI130Checkers_';
 $dbName = 'gameData';
 
-$leaderboard = [];
-
 $conn = new mysqli($servername, $username, $password, $dbName);
 if($conn->connect_error){
     die("Error: " . $conn->connect_error);
 }
-$size = "SELECT COUNT(*) FROM users";
-$sizeResult = $conn->query($size);
-$sizeRow = $sizeResult->fetch_row();
 
-$playerData = "SELECT * FROM users";
-$leaderboardResult = $conn->query($playerData);
+$leaderboard = [];
+$leaderboardData = "SELECT * FROM users";
+$leaderboardResult = $conn->query($leaderboardData);
 
 while($leaderboardRow = $leaderboardResult->fetch_assoc()){
     $leaderboard[] = $leaderboardRow;
